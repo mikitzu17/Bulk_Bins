947efd19ba8984eb1c40012192ccdf2bf9d39322
mikitzu17 <monica3214b@gmail.com>
2026-02-20 16:38:59 +0530

    use environment variable for API URL

--- frontend/src/services/aiApi.js
+++ frontend/src/services/aiApi.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000/api';
+//const API_URL = 'http://localhost:5000/api';
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export const uploadCsvForAnalysis = async (businessId, file) => {
     const token = localStorage.getItem('token');
