abfd60a599a3a4e78b92b3086a69b52eb2869832
mikitzu17 <monica3214b@gmail.com>
2026-02-21 12:34:05 +0530

    fixed smtp connection using ssl port 465

--- backend/app.py
+++ backend/app.py
@@ -52,8 +52,9 @@ create_tables()
 # Flask-Mail Configuration
 from flask_mail import Mail
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 465))
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', app.config['MAIL_USERNAME'])
